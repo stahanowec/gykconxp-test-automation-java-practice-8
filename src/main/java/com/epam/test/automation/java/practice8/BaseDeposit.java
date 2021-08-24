bcfc4fe683344078473649b8a3373f85c0d1c1f4
API Client <api-client@example.com>
2021-08-24 20:28:02 +0000

    Initial commit

---
+++ src/main/java/com/epam/test/automation/java/practice8/BaseDeposit.java
@@ -0,0 +1,10 @@
+package com.epam.test.automation.java.practice8;
+
+/**
+ * <summary>
+ * Implement class according to description of task.
+ * </summary>
+ */
+public class BaseDeposit {
+    //TODO: Delete this line and write your own solution;
+}
